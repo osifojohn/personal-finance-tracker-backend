44dd7fd5ae39595b3d3d04dc822296c9a3c7b83e
Osifo John <osifojohntec@gmail.com>
2023-06-09 22:25:44 +0100

    added user jpa repository

---
+++ src/main/java/com/osifojohncode/financetracker/entity/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.osifojohncode.financetracker.entity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
