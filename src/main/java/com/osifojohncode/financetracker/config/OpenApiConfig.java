f8b2e4f1a06dfcd505a75e16361cfa59f037dd36
Osifo John <osifojohntec@gmail.com>
2023-06-20 00:01:42 +0100

    implemented OpenApi Documentation | Swagger UI for register and authenticate

---
+++ src/main/java/com/osifojohncode/financetracker/config/OpenApiConfig.java
@@ -0,0 +1,48 @@
+package com.osifojohncode.financetracker.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Osifo John",
+                        email = "osifojohntec@gmail.com",
+                        url = "https://osifojohn.netlify.app/"
+                ),
+                description = "OpenApi documentation for personal-finance-tracker",
+                title = "OpenApi specification - Osifo John",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://osifojohn.netlify.app/"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/osifojohncode/financetracker/controller/AuthenticationController.java
+++ src/main/java/com/osifojohncode/financetracker/controller/AuthenticationController.java
@@ -6,6 +6,9 @@ import com.osifojohncode.financetracker.dto.RegisterRequest;
 import com.osifojohncode.financetracker.error.BadCredentialsException;
 import com.osifojohncode.financetracker.error.UserAlreadyExistsException;
 import com.osifojohncode.financetracker.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +16,46 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication and Login")
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    @Operation(
+            description = "Post endpoint for Register",
+            summary = "This is a summary for Register post endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Email already taken",
+                            responseCode = "409"
+                    )
+            }
 
+    )
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) throws UserAlreadyExistsException {
         return ResponseEntity.ok(service.register(request));
     }
+    @Operation(
+            description = "Post endpoint for Authenticate",
+            summary = "This is a summary for Authentication post endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid token",
+                            responseCode = "403"
+                    )
+            }
 
+    )
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
